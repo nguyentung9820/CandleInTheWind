ceb07ee3c4a5c525041cb3ba646cd4446fa90c89
DUNG NGUYEN TIEN <DUNG.NT181059@sis.hust.edu.vn>
2021-11-11 20:56:27 +0700

    get data comment

--- src/Blog/app/controller/blog/BlogController.js
+++ src/Blog/app/controller/blog/BlogController.js
@@ -6,25 +6,70 @@ class BlogController {
     // [GET] /
     homepage(req,res,next)
     {
-        let savePosts
-        let saveCmts
-        savePosts= Post.find({})
+        let savePosts = []
+        let saveCmts= []
+        let arrPosts= []
+        Post.find({})
         .then(posts =>
         {        
-            posts= multipleMongoObj(posts) 
-            
-                   
+            savePosts= multipleMongoObj(posts)                  
         })
-        .then(console.log(savePosts))
+        .then( 
+            CommentModel.find({})
+             .then(cmt => {
+            cmt = multipleMongoObj(cmt)
+            savePosts.forEach(element => {
+                const cmts = []
+                cmt.forEach(id =>
+                    {
+                        if(id.postID==element.postID)
+                        cmts.push(id.caption)
+                    })
+                arrPosts.push({
+                    caption: element.caption,
+                    arrCmt: cmts
+                })
+            });
+        })
+        .then(tmp=> {      
+         res.render("templates/blog/blog",{arrPosts})
+         console.log(arrPosts);   
+        })
+        )
         .catch(next)
 
-        saveCmts=CommentModel.find({})
-        .then(cmt => {
-            cmt = multipleMongoObj(cmt)
-        })
+
+       
+
+        // Post.find({})
+        // .then((posts) => {
+        //   let elementsArr = [];
+        //   let cmtsArr = [];
+        //   let postArr = [];
+      
+        //   posts = multipleMongoObj(posts);
+        //   posts.forEach((element) => {
+        //     CommentModel.find({ postID: element.postID }).then((cmts) => {
+        //       elementsArr.push(element.caption);
+        //       cmtsArr.push(cmts);
+        //     });
+        //   });
+      
+        //   res.render("templates/blog/blog",  {
+        //     elements: elementsArr, //! mảng chứa các elements đã lấy trong forEach
+        //     cmt: cmtsArr, //! mảng chứa các cmt đã lấy trong forEach
+        //     post: postArr, //! chứa post
+        //   });
+        // })
+        // .catch(next);
 
     }
      
+    ListPost(post,cmt)
+    {
+
+    }
+
 
     Comment(req,res,next)
     {        
@@ -38,6 +83,9 @@ class BlogController {
     }
 }
 
-
+class ArrayPost{
+    caption
+    cmt = new Array()
+}
 
 module.exports = new BlogController;
